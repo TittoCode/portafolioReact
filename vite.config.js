5030c05865933a8244f174eee13eee40b45a08df
TittoCode <jtittoy@gmail.com>
2026-03-25 12:26:26 -0400

    fix(deploy): configurar base url '/portafolioReact/' para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Si vas a desplegar en https://<usuario>.github.io/<nombre-del-repo>/
-  // descomenta la siguiente línea y reemplaza con el nombre de tu repositorio:
-  // base: '/npm_del_repo/',
+  // Configurado para GitHub Pages
+  base: '/portafolioReact/',
 })
